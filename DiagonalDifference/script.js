2a8bb7bff4fea787c496138ad26856b3938e3597
Danpowers24 <danpowers610@gmail.com>
2020-08-21 17:47:22 -0700

    Finish diagonaldiffernece solution

--- DiagonalDifference/script.js
+++ DiagonalDifference/script.js
@@ -1,44 +1,26 @@
-// in a 4x4 matrix, Array.length = 15
-// to find the diagnol of any given size matrix, 
-// matrix size ---  n = square root of (array.length + 1)
-// to traverse the matrix diagonally, we need to find every
-
 
 let arr = [
-    [1,
-    2,
-    3,],
-    [4,
-    5,
-    6,],
-    [7,
-    8,
-    9]
+    [1,2,3,],
+    [4,5,6,],
+    [7,8,9]
   ];
-  // let newAr = ar.split(' ');
-  //     let result = 0
-  //     console.log("ar is ",  newAr);
-  //     let newerAr = newAr.map(item => parseInt(item))
-  //     console.log(newerAr)
-  // let arr = '11 2 4 4 5 6 10 8 -12';
+  
   function diagonalDifference(arr) {
-      console.log(arr)
-    let newArr = arr.flat();
-    console.log(newArr);
-    // let newAr = arr.split(' ');
-    // let newerAr = newAr.map((item) => parseInt(item));
-    let n = Math.sqrt(newArr.length);
-    let countOne = 0;
-    let countTwo = 0;
-    for (let i = 0; i <= newArr.length; i += n + 1) {
-      countOne += newArr[i];
-      console.log(countOne);
+    let firstDiag = 0
+    let secondDiag = 0
+    // push in values of first diagonal
+    for (let i = 0; i < arr.length; i++) {
+      // push in arr[i][i] into firstDiag
+      firstDiag += arr[i][i]
     }
-    for (let j = n - 1; j <= newArr.length - n; j += n - 1) {
-      countTwo += newArr[j];
-      console.log(countTwo);
-    }
-    let result = Math.abs(countOne - countTwo);
-    return result;
+    // push in values of second diagonal
+    let z = arr.length - 1
+      for (let j = 0; j < arr.length; j++) {
+        secondDiag += arr[j][z]
+        z--
+      }
+    return Math.abs(firstDiag - secondDiag)
   }
+
   diagonalDifference(arr);
+
